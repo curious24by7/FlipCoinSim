7dacd36b3c9667e9e5e08bb55541a4c5bd523d01
sohamdhamnaskar <sohamdham123@gmail.com>
2021-03-27 16:43:36 +0530

    Added: How many times Head or Tail win.

--- flipcoinsim.sh
+++ flipcoinsim.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -x
 
-#variables
-isHeads=1
-isTails=0
+echo ENTER NUMBER OF TIMES YOU WANT TO FLIP A COIN::
+read n
 
-if [ $((RANDOM%2)) -eq $isHeads ]
-	then
-	echo	"Its Heads"
-	else
-	echo	"Its Tails"
+#variables
+h=0
+t=0
+
+while [ $h -le $n ] && [ $t -le $n ]
+do
+		flip=$((RANDOM%2))
+
+	if [ $flip -eq 1 ]
+		then ((h++))
+		echo Heads
+				Heads=$(($h+1))
+		else ((t++))
+		echo Tails
+				Tails=$(($t+1))
+	fi
+done
+
+if [ $Heads -gt $Tails ]
+then
+echo HEAD WINS.
+else
+echo TAIL WINS.
 fi
